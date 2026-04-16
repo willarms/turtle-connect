08a22352c0ba5081703599be90701b07636d5cc0
Will Armsworthy <warmswor@nd.edu>
2026-04-16 11:04:58 -0400

    Small bug fixes

--- frontend/src/pages/GroupDetail.jsx
+++ frontend/src/pages/GroupDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { createMeetLink, getGoogleAuthorizeUrl, getGroup, joinGroup } from '../services/api'
 
